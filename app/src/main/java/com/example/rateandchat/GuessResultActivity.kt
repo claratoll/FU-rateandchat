7e5f9c9bc0c375d40d54ad72503b72a385694c59
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2023-02-14 13:29:43 +0100

    More commit guess design

---
+++ app/src/main/java/com/example/rateandchat/GuessResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rateandchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuessResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guess_result)
+    }
+}
--- app/src/main/java/com/example/rateandchat/SportsActivity.kt
+++ app/src/main/java/com/example/rateandchat/SportsActivity.kt
@@ -1,11 +1,19 @@
 package com.example.rateandchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SportsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sports)
+    }
+
+    //skapa bara för att prova gå vidare till gissning
+    fun testToGuessActivity (view : View){
+        val intent = Intent(this, GuessResultActivity::class.java)
+        startActivity(intent)
     }
 }
