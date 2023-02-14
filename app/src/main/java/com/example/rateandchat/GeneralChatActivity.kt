6348aa5d5cfdfdc0667d3844bf757ce0555b8ae7
Antonio <antonio.saracino83@gmail.com>
2023-02-14 23:03:55 +0100

    Group chat prototype. Needs testing.

---
+++ app/src/main/java/com/example/rateandchat/GeneralChatActivity.kt
@@ -0,0 +1,87 @@
+package com.example.rateandchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rateandchat.dataclass.Message
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+class GeneralChatActivity : AppCompatActivity() {
+
+    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageBox : EditText
+    private lateinit var sendButton : ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var uidRef : CollectionReference
+    private lateinit var db : FirebaseFirestore
+    private lateinit var messagesRef : CollectionReference
+
+    var receiverRoom : String? = null
+    var generalRoom : String = "dummychat"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+        val name = intent.getStringExtra("name")
+
+        db = Firebase.firestore
+        messagesRef = db.collection("Chats")
+        uidRef = db.collection("Users")
+
+        supportActionBar?.title = name
+
+        chatRecyclerView = findViewById(R.id.chatRecycleView)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sendButton)
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
+        chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        chatRecyclerView.adapter = messageAdapter
+
+        messagesRef.document(generalRoom).collection("Messages")
+            .orderBy("timestamp")
+            .addSnapshotListener { snapshot, e ->
+                messageList.clear()
+                if (snapshot != null) {
+                    for (document in snapshot.documents) {
+                        val item = document.toObject<Message>()
+                        messageList.add(item!!)
+                    }
+                    messageAdapter.notifyDataSetChanged()
+                }
+            }
+
+        // adding the message to database
+        sendButton.setOnClickListener {
+
+            val message = messageBox.text.toString()
+            val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+            val messageObject = Message(message, senderUid)
+
+            messagesRef.document(generalRoom).collection("Messages")
+                .add(messageObject)
+                .addOnSuccessListener { documentReference ->
+                    messagesRef.document("$receiverRoom").collection("Messages")
+                        .add(messageObject)
+                    Log.d("msg", "DocumentSnapshot written with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.d("msg", "Error adding document", e)
+                }
+
+            messageBox.setText("")
+
+        }
+    }
+}
--- app/src/main/java/com/example/rateandchat/UserListActivity.kt
+++ app/src/main/java/com/example/rateandchat/UserListActivity.kt
@@ -1,11 +1,13 @@
 package com.example.rateandchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rateandchat.dataclass.User
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
@@ -21,6 +23,7 @@ class UserListActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
     private lateinit var db : FirebaseFirestore
     private lateinit var usersRef : CollectionReference
+    private lateinit var generalChatFab : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,9 +40,15 @@ class UserListActivity : AppCompatActivity() {
         adapter = UserAdapter(this, userList)
 
         userRecyclerView = findViewById(R.id.userRecyclerView)
+        generalChatFab = findViewById(R.id.generalChatFab)
 
         userRecyclerView.layoutManager = LinearLayoutManager(this)
         userRecyclerView.adapter = adapter
+
+        generalChatFab.setOnClickListener {
+            val intent = Intent(this, GeneralChatActivity::class.java)
+            startActivity(intent)
+        }
 
         usersRef.addSnapshotListener() { snapshot, e ->
             if (snapshot != null) {
